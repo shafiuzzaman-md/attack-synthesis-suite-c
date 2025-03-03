14b7877b837aa88ce102e4a80745dde662692171
Md Shafiuzzaman <mdshafiuzzaman@ucsb.edu>
2025-03-03 10:40:57 -0800

    refactoring

--- effectfunctions/templates/cwe226_effect.py
+++ effectfunctions/templates/cwe226_effect.py
@@ -5,7 +5,7 @@ class SegmentIdentifier:
     def __init__(self, segment_name: str):
         self.segment_name = segment_name
 
-def CWE226_Sensitive_InformationUnclearedBeforeRelease_bad(
+def CWE226_Sensitive_InformationUnclearedBeforeRelease(
     memory: MemoryState,
     memory_segment: SegmentIdentifier,
     required_permissions: Permissions,
